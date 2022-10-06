5f423d3a26590fb664e5d8c6c9f7de429c995f85
Micheal Dutt <31451034+michealdutt@users.noreply.github.com>
2022-10-06 17:20:05 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#run all the files to generate models
 import os
 
 list_run = ["run_eeg_fpz_cz_crf.py", "run_eeg_fpz_cz_cnn.py", "run_eeg_pz_oz_cnn.py", "run_eeg_pz_oz_crf.py"] 
@@ -5,3 +6,6 @@ list_run = ["run_eeg_fpz_cz_crf.py", "run_eeg_fpz_cz_cnn.py", "run_eeg_pz_oz_cnn
 for file in list_run:
     print(file)
     os.system("python " + str(file))
+
+    
+    
