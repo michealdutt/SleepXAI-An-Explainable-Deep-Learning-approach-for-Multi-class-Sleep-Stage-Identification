b874c4daf3741a18e86c040ffc50ec64be6fa89f
Micheal Dutt <31451034+michealdutt@users.noreply.github.com>
2022-10-11 12:38:42 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import os
 
 list_run = ["run_eeg_fpz_cz_crf.py", "run_eeg_fpz_cz_cnn.py", "run_eeg_pz_oz_cnn.py", "run_eeg_pz_oz_crf.py"] 
 
+
+#run all the files
 for file in list_run:
     print(file)
     os.system("python " + str(file))
